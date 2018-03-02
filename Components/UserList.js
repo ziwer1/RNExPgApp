695585933091670436069b3b362f80abd4549c74
ziwer1 <ziwer1@gmail.com>
2018-03-03 00:14:27 +0300

    initial commit

---
+++ Components/UserList.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import {Stylesheet, Text, View} from 'react-native'
+
+export default class UserList extends React.Component{
+
+	render(){
+		return(
+			<View>
+				<Text>This is the user list page</Text>
+			</View>
+		)
+	}
+}
---
+++ Components/UserProfile.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import {Stylesheet, View, Text} from 'react-native'
+
+export default class UserProfile extends React.Component{
+
+	render(){
+		return(
+			<View>
+				<Text>The user profile page.</Text>
+			</View>
+		)
+	}
+}
